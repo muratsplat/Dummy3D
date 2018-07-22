164366bdaab097af121aa5a3f9e6bb51c8d32342
Murat Ödünç <murat.asya@gmail.com>
2018-07-22 15:29:34 +0300

    init  xunit

--- Models/Response/ResEnrolled.cs
+++ Models/Response/ResEnrolled.cs
@@ -9,13 +9,3 @@ namespace Dummy3D.Models.Response {
  
     }
 }
-
-
-
-// {
-//     "acs_url":"https://secure5.arcot.com/acspage/cap?RID=35325&VAA=B",
-//     "pareq":"eNpdU8tymzAU3ecrvMumYz1AgD2yZnDsTpMZx06TRdudLK5skhiIgNjust/Tr+qXVMIm4DDDoHvuOdKZexB/2hqA2SOo2oC4Ggz4AspSbmCQJpPrSELAZKApSOyFkWYaY+brJAq0pzTQa6ewmlX8Hd4aRZDoKMIs8WXiK2/NvIAFDPwwoRrCkV6fFVbzDqZM80yQIR5SjtqybRcyE4xFPmUhoZiGhPkRRw5tGQswaiuzqgUsJNXb9PZeMBpYFUfnsuvvwNzOBKa4fTg6QR0lkzsQ84PcFa/AUVN1TZXXWWWOgmLrpS26dm1exbaqijFC+/1+CKddhrnZII5cs7WOPnvnq9oBZf+wQ5qIxSzed+/8uPj9QO6flb98iiccOUbHT2QF1hlhmODRgARj6o0Z46jBezPaOd/i35+/hH7x7ATOQMconJf4hBLqKH2kN43aGMjUUYxCN4626ghwKPIMrMbm+7HuGYZSCevPfT4m83kQ/ObbRcCqsnHls7V++fVAHoPlnb/9eVMuVz+m8fzrNN5MXOwN6cJHaoMiETkZSbvUOGr3t0e7v7i5A+h8CcQVR5cX5D/FxN2u",
-//     "enrolled":"Y",
-//     "eci":"2",
-//     "error":null
-// }
